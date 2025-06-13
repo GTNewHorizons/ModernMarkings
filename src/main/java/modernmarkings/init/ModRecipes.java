592cce0feeddaaf5f41cb6e05a1b85fa58a4dbfd
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-06-13 09:49:22 +0200

    Revert previous PR (#15)

--- src/main/java/modernmarkings/init/ModRecipes.java
+++ src/main/java/modernmarkings/init/ModRecipes.java
@@ -31,15 +31,6 @@ public class ModRecipes {
             Items.paper,
             'Y',
             Blocks.planks);
-        GameRegistry.addRecipe(
-            new ItemStack(ModBlocks.WALL_MARKING_FLAG_PRIDE, 64),
-            "   ",
-            "YX ",
-            "Y  ",
-            'X',
-            Items.paper,
-            'Y',
-            Items.stick);
         addChiselRecipes();
     }
 
@@ -49,14 +40,9 @@ public class ModRecipes {
             CarvingUtils.chisel.addVariation("Floor blocks", block, 0, i);
         }
 
-        for (int i = 0; i < ModBlocks.WALL_BLOCKS_GENERAL.size(); i++) {
-            Block block = ModBlocks.WALL_BLOCKS_GENERAL.get(i);
+        for (int i = 0; i < ModBlocks.WALL_BLOCKS.size(); i++) {
+            Block block = ModBlocks.WALL_BLOCKS.get(i);
             CarvingUtils.chisel.addVariation("Wall blocks", block, 0, i);
-        }
-
-        for (int i = 0; i < ModBlocks.WALL_BLOCKS_FLAG.size(); i++) {
-            Block block = ModBlocks.WALL_BLOCKS_FLAG.get(i);
-            CarvingUtils.chisel.addVariation("Flags", block, 0, i);
         }
     };
 
